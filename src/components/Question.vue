7f47c7c4689a219571fc5ece1252cbdfd5cbdd11
spiroskarathanassis <skarathanassisdev@gmail.com>
2021-01-04 16:10:31 +0200

    remove useless files

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,65 +1,65 @@
 <template>
-    <div v-if="!isQuizStarted" class="quiz-title">{{questionData.title}}</div>
-    <div v-else>
-      <div v-if="!isQuizFinished" id="questionNumber" class="question-number">Question {{currentQuestion + 1}} / {{ totalQuestions }}</div>
-      <div :class="{'result-title': isQuizFinished}">{{ question.title }} 
-        <strong v-if="question.type === 'mutiplechoice-multiple'" title="mutiplechoice question">*</strong>
-      </div>
-      <img :src="question.imageSrc" alt="" />
-      <p class="message" v-if="isQuizFinished">{{ this.message.text }}</p>
-      <p class="message" v-else-if="message.isEnabled">{{ message.isAnswerCorrect ? 'Correct Answer' : 'Wrong Answer' }}</p>
-      <div v-if="!isQuizFinished" id="answers" class="container-answers">
-        <button 
-          v-for="(ans, index) in question.answers"
-          :key="question.type === 'truefalse' ? index : ans.a_id"
-          :value="index"
-          data-selected="false"
-          :data-id="question.type === 'truefalse' ? !index : ans.a_id"
-          @click="this.toggleAnswers"
-          :class="[
-            {correct: answer.isCorrect && answer.classNames[index]}, 
-            {wrong: !answer.isCorrect && answer.classNames[index]}, 
-            {active: isAnswerActive[index]}
-          ]"
-          class="answer-btn">{{ question.type !== 'truefalse' ? ans.caption : ans }}</button>
-      </div>
-      <div v-else class="score-message">Your score is {{ totalScore }} %</div>
-    </div>
-    <div class="conrtols" :class="{'start-ctl': !isQuizStarted}">
-      <button v-if="!isQuizStarted && !isQuizFinished"
-        @click="isQuizStarted = true" 
-        class="tool-btn start">Start</button>
-      <button v-else-if="isQuizStarted && !isQuizFinished"
-        @click="setNextQuestion" 
-        :disabled="isBtnDisabled"
-        class="tool-btn">{{ (currentQuestion != (totalQuestions - 1)) ? 'Next' : 'Finish' }}</button>
-      <button v-else
-        @click="resetQuiz"
-        class="tool-btn start">Play Again</button>
-    </div>
+  <div class="question-number">Question {{question.currentIndex + 1}} / {{ totalQuestions }}</div>
+  <div>{{ question.title }}
+    <strong 
+      v-show="question.type === 'mutiplechoice-multiple'" 
+      title="mutiplechoice question">*</strong>
+  </div>
+  <img :src="question.imageSrc" alt="" />
+  <p 
+    v-show="message.isEnabled"
+    class="message"
+  >{{ message.isAnswerCorrect ? 'Correct Answer' : 'Wrong Answer' }}</p>
+
+  <div id="answers" class="container-answers">
+    <button 
+      v-for="(ans, index) in question.possibleAnswers"
+      :key="question.type === 'truefalse' ? index : ans.a_id"
+      :value="index"
+      :data-id="question.type === 'truefalse' ? !index : ans.a_id"
+      data-selected="false"
+      @click="this.toggleAnswers"
+      :class="[
+        {correct: answer.isCorrect && answer.classNames[index]}, 
+        {wrong: !answer.isCorrect && answer.classNames[index]}, 
+        {active: isAnswerActive[index]}
+      ]"
+      class="answer-btn"
+    >{{ question.type !== 'truefalse' ? ans.caption : ans }}</button>
+  </div>
+
+  <div class="conrtols">
+    <button
+      @click="setNextQuestion" 
+      :disabled="isBtnDisabled"
+      class="tool-btn"
+    >{{ (question.currentIndex !== (totalQuestions - 1)) ? 'Next' : 'Finish' }}</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Question',
+  props: ['quizData'],
+  emits: ['print-results'],
   data () {
     return {
-      questionData: '',
+      totalQuestions: null,
+      previousAnswerIndex: null,
+      isAnswerActive: [], //all answers indexing by loop 1-4
+      isBtnDisabled: true,
       points: {
         player: 0,
         totalGame: 0
       },
-      currentQuestion: 0,
-      totalQuestions: null,
       question: {
+        currentIndex: 0,
         type: '',
         title: '',
         imageSrc: '',
         points: '',
-        answers: []
+        possibleAnswers: []
       },
-      isAnswerActive: [], //all answers indexing by loop 1-4
-      previousAnswerIndex: null,
       message: {
         isEnabled: false,
         isAnswerCorrect: false,
@@ -69,32 +69,22 @@ export default {
         classNames: [],
         isCorrect: false,
       },
-      isBtnDisabled: true,
-      isQuizFinished: false,
-      isQuizStarted: false,
-      totalScore: 0
     }
   },
-  async created () {
-    await this.fetchQuizData(false); //questions
-    this.totalQuestions = this.questionData.questions.length;
+  created () {
+    this.totalQuestions = this.quizData.length;
     this.displayQuestion();
   },
-  watch: {
-    '$route': 'fetchQuizData'
-  },
-  computed: {
-
-  },
   methods: {
-    async fetchQuizData(isGameFinished) {
-      const board = isGameFinished ? 'results' : 'questions';
-      const baseUrl = 'https://apis-game-default-rtdb.europe-west1.firebasedatabase.app/apis-game-default-rtdb/';
-
-      this.questionData = await fetch(baseUrl + board + '.json')
-        .then(res => res.json())
-        .then(res => res)
-        .catch(e => console.error(e.message));
+    displayQuestion() {
+      const questionIndex = this.quizData[this.question.currentIndex];
+      this.question.type = questionIndex.question_type;
+      this.question.title = questionIndex.title;
+      this.question.imageSrc = require('@/assets/' + questionIndex.img);
+      this.question.possibleAnswers = (this.question.type === 'truefalse') 
+        ? ['True', 'False'] 
+        : questionIndex.possible_answers;
+      this.question.points = questionIndex.points;
     },
     async setNextQuestion() {
       this.isBtnDisabled = true;
@@ -103,19 +93,8 @@ export default {
       this.postMessage(isCorrect);
       this.displayCorrectAnswers(isCorrect);
       
-      const isFinished = this.currentQuestion >= (this.totalQuestions - 1);
+      const isFinished = this.question.currentIndex >= (this.totalQuestions - 1);
       await this.stopTime(isFinished);
-    },
-    displayQuestion() {
-      // console.log(this.questionData);
-      const questionIndex = this.questionData.questions[this.currentQuestion];
-      this.question.type = questionIndex.question_type;
-      this.question.title = questionIndex.title;
-      this.question.imageSrc = require('@/assets/' + questionIndex.img);
-      this.question.answers = (this.question.type === 'truefalse') 
-        ? ['True', 'False'] 
-        : questionIndex.possible_answers;
-      this.question.points = questionIndex.points;
     },
     postMessage(isCorrect) {
       this.message.isEnabled = true;
@@ -141,10 +120,10 @@ export default {
       await setTimeout(() => {
         this.uncheckAnswers();
         if (!isFinished) {
-          ++this.currentQuestion;
+          ++this.question.currentIndex;
           this.displayQuestion();
         } else {
-          this.setResults();
+          this.prepareResults();
         }
       }, 3000);
     },
@@ -171,22 +150,22 @@ export default {
     },
     compareAnswers() {
       if (this.question.type !== 'mutiplechoice-multiple') {
-        const correctAnswer = this.questionData.questions[this.currentQuestion].correct_answer;
+        const correctAnswer = this.quizData[this.question.currentIndex].correct_answer;
         const answerIndex = this.isAnswerActive.findIndex(el => el === true);
 
         if (this.question.type === 'truefalse') return !answerIndex == correctAnswer;
-        return (this.question.answers[answerIndex].a_id === correctAnswer);
+        return (this.question.possibleAnswers[answerIndex].a_id === correctAnswer);
       }
 
       // case mutiplechoice-multiple below
-      const correctAnswers = this.questionData.questions[this.currentQuestion].correct_answer;
+      const correctAnswers = this.quizData[this.question.currentIndex].correct_answer;
       let sameAnswers = 0;
       const numberOfAnswered = this.isAnswerActive.filter(el => el === true).length;
       
       correctAnswers.forEach(ans => {
         this.isAnswerActive.map((el, index) => {
           if (el === true) {
-            if (this.question.answers[index].a_id === ans) sameAnswers++;
+            if (this.question.possibleAnswers[index].a_id === ans) sameAnswers++;
           }
 
           return el;
@@ -202,9 +181,9 @@ export default {
       let correctAnswers = [];
 
       if (this.question.type === 'mutiplechoice-multiple') {
-        correctAnswers = this.questionData.questions[this.currentQuestion].correct_answer;
+        correctAnswers = this.quizData[this.question.currentIndex].correct_answer;
       } else {
-        correctAnswers[0] = this.questionData.questions[this.currentQuestion].correct_answer;
+        correctAnswers[0] = this.quizData[this.question.currentIndex].correct_answer;
       }
 
       buttons.forEach((button, index) => {
@@ -215,39 +194,19 @@ export default {
         });
       });
     },
-    async setResults() {
-      let results;
+    prepareResults() {
+      let results = null;
+      let errorMsg = null;
+      
       try {
         results = Math.floor(this.points.player * 100 / this.points.totalGame);
       } catch (e) {
         console.log(e.message);
+        errorMsg = 'Something went wrong with game points';
       }
 
-      await this.fetchQuizData(true); //results
-      this.displayResults(results);
+      this.$emit('print-results', {results, errorMsg});
     },
-    displayResults(results) {
-      this.isQuizFinished = true;
-      
-      const currentResult = this.questionData.results.find(el => 
-        (results >= (el.minpoints - 1)) && (results <= el.maxpoints)
-      );
-
-      this.question.title = currentResult.title;
-      this.message.text = currentResult.message;
-      this.totalScore = results;
-    },
-    async resetQuiz() {
-      this.isQuizFinished = false;
-      this.message.isEnabled = false;
-      this.currentQuestion = 0;
-      await this.fetchQuizData(false); //questions
-      this.displayQuestion();
-    }
   },
 }
 </script>
-
-<style lang="scss">
-  @import "../app.scss";
-</style>
